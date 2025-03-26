6e3fb80ad8561ab3561714bb85cc6b929ba0c4ac
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-03-27 00:20:56 +0200

    add address to ALLOWED_HOSTS

--- task_manager/settings/dev.py
+++ task_manager/settings/dev.py
@@ -3,7 +3,12 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", False) != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "planner-application.onrender.com"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "planner-application.onrender.com",
+    os.getenv("RENDER_EXTERNAL_HOSTNAME", ""),
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
