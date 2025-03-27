ad37fcbc285a78861cca99546bfaa8d39c0a1960
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-03-27 15:59:44 +0200

    add address to README.md

--- task_manager/settings/dev.py
+++ task_manager/settings/dev.py
@@ -3,12 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() in ("true", "1")
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "planner-application.onrender.com",
-    os.getenv("RENDER_EXTERNAL_HOSTNAME", ""),
-]
+ALLOWED_HOSTS = ["*"]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
